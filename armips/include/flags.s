deebcd5704d5cfd70505167088c8b20a3f9ba137
kumakuma1 <kuehnmaxi@gmail.com>
2024-02-24 11:47:05 +0100

    DSPRE edits

--- armips/include/flags.s
+++ armips/include/flags.s
@@ -141,7 +141,7 @@ FLAG_TRADE_VIOLET_CITY_BELLSPROUT_ONIX                     equ  0x71
 FLAG_UNK_072                                               equ  0x72
 FLAG_GOT_TM51_FROM_FALKNER                                 equ  0x73
 FLAG_UNK_074                                               equ  0x74
-FLAG_GOT_OLD_ROD                                           equ  0x75
+FLAG_GOT_OLD_ROD                                           equ  0x75 // unused
 FLAG_UNK_076                                               equ  0x76
 FLAG_UNK_077                                               equ  0x77
 FLAG_UNK_078                                               equ  0x78
@@ -190,7 +190,7 @@ FLAG_GOT_HM03                                              equ  0xA2
 FLAG_GOT_DOWSING_MACHINE                                   equ  0xA3
 FLAG_ENGAGING_STATIC_POKEMON                               equ  0xA4
 FLAG_GOT_MAGNET_FROM_SUNNY                                 equ  0xA5
-FLAG_GOT_TM30_FROM_MORTY                                   equ  0xA6
+FLAG_GOT_TM30_FROM_MORTY                                   equ  0xA6 // used to block Alph Ruins Union Cave entry
 FLAG_TALKED_TO_MOM_AFTER_NAMING_RIVAL                      equ  0xA7
 FLAG_UNK_0A8                                               equ  0xA8
 FLAG_UNK_0A9                                               equ  0xA9
