b5479322f8c81da226fee30c8e163052f909e934
kumakuma1 <64742333+kumakuma1@users.noreply.github.com>
2024-03-12 15:13:59 +0100

    Add files via upload

---
+++ armips/asm/custom/encounter_rates.s
@@ -0,0 +1,58 @@
+.nds
+.thumb
+
+// Adjust encounter rates
+
+// These work by generating a number 0-99, then doing a load of >= checks
+// The last one is == 99 so this has to be changed
+
+.open "base/overlay/overlay_0002.bin", 0x02245B80
+
+// Surfing Encounters
+// Change rates to 25/25/25/20/5
+
+.org 0x02247734
+
+// 25%
+.byte 25
+.skip 9
+
+// 25%
+.byte 50
+.skip 7
+.byte 50
+.skip 3
+
+// 25%
+.byte 75
+.skip 7
+.byte 75
+.skip 3
+
+// 20%
+// then values 90 - 99 activate last slot
+.byte 95
+
+// Fishing Encounters
+// Change rates to 25/25/25/20/5
+
+.org 0x02247778
+
+// 25%
+.byte 25
+.skip 7
+
+// 25%
+.byte 50
+.skip 7
+
+// 25%
+.byte 75
+.skip 7
+
+// 20%
+// then values 90 - 99 activate last slot
+.byte 95
+
+
+.close
