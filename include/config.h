37d894f77907bbce026c6ff5ef5a75027372dded
kumakuma1 <kuehnmaxi@gmail.com>
2025-06-04 21:08:52 +0200

    Revert "Merge remote-tracking branch 'origin/main' into HardloveGoldRom"
    This reverts commit b34c53638a94a0ad89b0837c7cfc1d32b67391b1, reversing
    changes made to 44c65a99aa024397c7f3e3a62d35d10aeeb74846.

--- include/config.h
+++ include/config.h
@@ -78,8 +78,7 @@
 
 // IMPLEMENT_LEVEL_CAP defines whether or not a configurable hard level cap system is built into the rom based on the value in LEVEL_CAP_VARIABLE
 // if the level is greater than or equal to LEVEL_CAP_VARIABLE, the pokémon will no longer gain experience
-// uncommenting IMPLEMENT_LEVEL_CAP enables the level cap system.  make sure to also uncomment LEVEL_CAP_VARIABLE in the process
-// uncommenting UNCAP_CANDIES_FROM_LEVEL_CAP will allow for rare candies to not be capped by the level cap even with the level cap in place, like run & bun
+// uncommenting IMPLEMENT_LEVEL_CAP enables the level cap system.  undefining LEVEL_CAP_VARIABLE will just cause compilation errors
 #define IMPLEMENT_LEVEL_CAP
 #define LEVEL_CAP_VARIABLE 0x416F
 #define UNCAP_CANDIES_FROM_LEVEL_CAP
@@ -120,14 +119,12 @@
 // modern generations have this value at 160, older ones at 220.  still max out at 255
 #define FRIENDSHIP_EVOLUTION_THRESHOLD 160
 
-
-// Friendship grants additional bonuses.
-// Comment out the line below to revert back to Gen 5- behaviour
-//#define FRIENDSHIP_EFFECTS
-
-// RESTORE_ITEMS_AT_BATTLE_END will restore held items that are single-use at the end of battle (Gen 9)
-// comment out the line below to revert back to Gen 8- behavior
+// RESTORE_ITEMS_AT_BATTLE_END will restore held items that are single-use at the end of battle
+// comment out the line below to keep vanilla behavior
 //#define RESTORE_ITEMS_AT_BATTLE_END
+
+// AI_CAN_GRAB_ITEMS allows to use Trick, Switcheroo, (Thief still todo) on the Player and actually grab items. This can result in lost items.
+#define AI_CAN_GRAB_ITEMS
 
 // AI_CAN_GRAB_ITEMS allows to use Trick, Switcheroo, (Thief still todo) on the Player and actually grab items. This can result in lost items.
 #define AI_CAN_GRAB_ITEMS
@@ -152,7 +149,7 @@
 #define UPDATE_VITAMIN_EV_CAPS
 
 // DISABLE_ITEMS_IN_TRAINER_BATTLE will disable the usage of items in trainer battles. This is also true for the AI.
-#define DISABLE_ITEMS_IN_TRAINER_BATTLE
+//#define DISABLE_ITEMS_IN_TRAINER_BATTLE
 
 // REUSABLE_TMS will make TMs infinite and hide the quantity number.
 #define REUSABLE_TMS
