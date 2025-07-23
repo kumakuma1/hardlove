bea116f2956f4857c79fdef69a66cca386888bcf
kumakuma1 <kuehnmaxi@gmail.com>
2025-07-23 14:41:36 +0200

    make weather/terrain infinite

--- src/individual/ServerFieldConditionCheck.c
+++ src/individual/ServerFieldConditionCheck.c
@@ -141,7 +141,7 @@ void ServerFieldConditionCheck(void *bw, struct BattleStruct *sp) {
                 sprintf(buf, "In ENDTURN_WEATHER_SUBSIDING\n");
                 debugsyscall(buf);
                 #endif
-
+                sp->fcc.weather_count = 1; //infinite weather
                 if (sp->field_condition & WEATHER_RAIN) {
                     if (--sp->fcc.weather_count == 0) {
                         LoadBattleSubSeqScript(sp, ARC_BATTLE_SUB_SEQ, SUB_SEQ_RAIN_END);
@@ -1552,7 +1552,7 @@ void ServerFieldConditionCheck(void *bw, struct BattleStruct *sp) {
                 #endif
 
                 if (sp->terrainOverlay.type != TERRAIN_NONE) {
-                    sp->terrainOverlay.numberOfTurnsLeft--;
+                    //sp->terrainOverlay.numberOfTurnsLeft--; // infinite terrain
                     if (sp->terrainOverlay.numberOfTurnsLeft <= 0) {
                         LoadBattleSubSeqScript(sp, ARC_BATTLE_SUB_SEQ, SUB_SEQ_HANDLE_TERRAIN_END);
                         sp->next_server_seq_no = sp->server_seq_no;
