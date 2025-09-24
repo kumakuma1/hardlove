91ab2fca08a7a843715fe6d60292d34a5e0eed70
kumakuma1 <kuehnmaxi@gmail.com>
2025-09-24 16:10:35 +0200

    berries up until 39

--- armips/scr_seq/scr_seq_0150.s
+++ armips/scr_seq/scr_seq_0150.s
@@ -22,6 +22,12 @@
 	scrdef scr_seq_0150_006 //r33
 	scrdef scr_seq_0150_007 //azalea
 	scrdef scr_seq_0150_008 //r36
+	scrdef scr_seq_0150_009 //r37
+	scrdef scr_seq_0150_010 //r37
+	scrdef scr_seq_0150_011 //r37
+	scrdef scr_seq_0150_012 //r38
+	scrdef scr_seq_0150_013 //r39
+	scrdef scr_seq_0150_014 //r35, surf
 	scrdef_end
 
 scr_seq_0150_000:
@@ -174,7 +180,121 @@ scr_seq_0150_008:
 	CommonScript 2008
 	//play_fanfare SEQ_ME_ITEM
 	//wait_fanfare
-	setflag 2577
+	setflag 2578
+	goto _goaway
+
+scr_seq_0150_009:
+	play_se SEQ_SE_DP_SELECT
+	lockall
+	faceplayer
+	npc_msg 10 //its  a berry tree
+	closemsg
+	goto_if_set 2579, _noberries
+	call _harvest
+	anim_apricorn_tree VAR_SPECIAL_RESULT
+	//GetRandom 0x800C, 50
+	setvar VAR_SPECIAL_x8004, ITEM_ROWAP_BERRY
+	setvar VAR_SPECIAL_x8005, 1
+	//IncrementVar VAR_SPECIAL_x8005, 0x800C 
+	CommonScript 2008
+	//play_fanfare SEQ_ME_ITEM
+	//wait_fanfare
+	setflag 2579
+	goto _goaway
+
+scr_seq_0150_010:
+	play_se SEQ_SE_DP_SELECT
+	lockall
+	faceplayer
+	npc_msg 10 //its  a berry tree
+	closemsg
+	goto_if_set 2580, _noberries
+	call _harvest
+	anim_apricorn_tree VAR_SPECIAL_RESULT
+	//GetRandom 0x800C, 50
+	setvar VAR_SPECIAL_x8004, ITEM_JABOCA_BERRY
+	setvar VAR_SPECIAL_x8005, 1
+	//IncrementVar VAR_SPECIAL_x8005, 0x800C 
+	CommonScript 2008
+	//play_fanfare SEQ_ME_ITEM
+	//wait_fanfare
+	setflag 2580
+	goto _goaway
+
+scr_seq_0150_011:
+	play_se SEQ_SE_DP_SELECT
+	lockall
+	faceplayer
+	npc_msg 10 //its  a berry tree
+	closemsg
+	goto_if_set 2581, _noberries
+	call _harvest
+	anim_apricorn_tree VAR_SPECIAL_RESULT
+	//GetRandom 0x800C, 50
+	setvar VAR_SPECIAL_x8004, ITEM_MICLE_BERRY
+	setvar VAR_SPECIAL_x8005, 2
+	//IncrementVar VAR_SPECIAL_x8005, 0x800C 
+	CommonScript 2008
+	//play_fanfare SEQ_ME_ITEM
+	//wait_fanfare
+	setflag 2581
+	goto _goaway
+
+scr_seq_0150_012:
+	play_se SEQ_SE_DP_SELECT
+	lockall
+	faceplayer
+	npc_msg 10 //its  a berry tree
+	closemsg
+	goto_if_set 2582, _noberries
+	call _harvest
+	anim_apricorn_tree VAR_SPECIAL_RESULT
+	GetRandom 0x800C, 30
+	setvar VAR_SPECIAL_x8004, ITEM_LEPPA_BERRY
+	setvar VAR_SPECIAL_x8005, 50
+	IncrementVar VAR_SPECIAL_x8005, 0x800C 
+	CommonScript 2008
+	//play_fanfare SEQ_ME_ITEM
+	//wait_fanfare
+	setflag 2582
+	goto _goaway
+
+scr_seq_0150_013:
+	play_se SEQ_SE_DP_SELECT
+	lockall
+	faceplayer
+	npc_msg 10 //its  a berry tree
+	closemsg
+	goto_if_set 2583, _noberries
+	call _harvest
+	anim_apricorn_tree VAR_SPECIAL_RESULT
+	GetRandom 0x800C, 30
+	setvar VAR_SPECIAL_x8004, ITEM_LUM_BERRY
+	setvar VAR_SPECIAL_x8005, 50
+	IncrementVar VAR_SPECIAL_x8005, 0x800C 
+	CommonScript 2008
+	//play_fanfare SEQ_ME_ITEM
+	//wait_fanfare
+	setflag 2583
+	goto _goaway
+
+scr_seq_0150_014:
+	play_se SEQ_SE_DP_SELECT
+	lockall
+	faceplayer
+	npc_msg 10 //its  a berry tree
+	closemsg
+	goto_if_set 2584, _noberries
+	call _harvest
+	anim_apricorn_tree VAR_SPECIAL_RESULT
+	GetRandom 0x800C, 30
+	setvar VAR_SPECIAL_x8004, ITEM_SITRUS_BERRY
+	setvar VAR_SPECIAL_x8005, 50
+	IncrementVar VAR_SPECIAL_x8005, 0x800C 
+	CommonScript 2008
+	//play_fanfare SEQ_ME_ITEM
+	//wait_fanfare
+	setflag 2584
 	goto _goaway
 
 _noberries:
