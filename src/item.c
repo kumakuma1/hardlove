e302b1b41a7129e6e615c86608fb40a258d68152
kumakuma1 <kuehnmaxi@gmail.com>
2025-10-03 14:46:09 +0200

    fix drowzee calm mind bug

--- src/item.c
+++ src/item.c
@@ -7,7 +7,7 @@
 #include "../include/constants/item.h"
 #include "../include/constants/moves.h"
 
-#define ITEM_DATA_MAX (ITEM_BRIARS_BOOK)
+#define ITEM_DATA_MAX (MAX_TOTAL_ITEM_NUM)
 #define GFX_ITEM_DUMMY_ID ((MAX_TOTAL_ITEM_NUM) * 2 + 2)
 #define GFX_ITEM_RETURN_ID ((MAX_TOTAL_ITEM_NUM+1) * 2 + 4)
 #define NEW_ITEM_GFX (797)
