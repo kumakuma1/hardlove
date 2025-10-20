592d7b86f0863126dae2bcb186f191fd7ac32afb
kumakuma1 <kuehnmaxi@gmail.com>
2025-10-20 16:52:45 +0200

    add surf without move encounter rates edits

--- armips/global.s
+++ armips/global.s
@@ -8,6 +8,10 @@
 .include "asm/include/moves.inc"
 .include "asm/include/species.inc"
 
+//custom
+.include "armips/asm/custom/enable_surf_without_knowing_move.s"
+.include "armips/asm/custom/encounter_rates.s"
+
 .include "armips/asm/backgrounds.s" // fixes new move animations
 .include "armips/asm/moves.s" // repoints all the move table defines within the structure
 .include "armips/asm/trainer_ai.s" // repoints all the move table defines for trainer ai within the structure
