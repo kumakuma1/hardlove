4e32dd007fce959bd96b300aac020eee0657b30f
kumakuma1 <kuehnmaxi@gmail.com>
2025-11-24 20:06:05 +0100

    good as gold fix abilities for trade mons with form

--- src/npc_trade.c
+++ src/npc_trade.c
@@ -50,6 +50,10 @@ void LONG_CALL _CreateTradeMon(struct PartyPokemon *mon, struct NPCTrade *trade_
             trade_dat->give_species = SPECIES_HERACROSS;
         } else {
             trade_dat->give_species = SPECIES_ARCANINE;
+            ability = ABILITY_INTIMIDATE;
+            if (gf_rand() % 2 == 0) {
+                ability = ABILITY_ROCK_HEAD;
+            }
         }
 
         trade_dat->heldItem = ITEM_SITRUS_BERRY;
@@ -62,7 +66,7 @@ void LONG_CALL _CreateTradeMon(struct PartyPokemon *mon, struct NPCTrade *trade_
     else if (tradeno == NPC_TRADE_SHUCKIE_SHUCKLE) {
         trade_dat->give_species = SPECIES_PIDGEY;
 
-        trade_dat->heldItem = ITEM_WACAN_BERRY;
+        trade_dat->heldItem = ITEM_YACHE_BERRY;
         trade_dat->gender = POKEMON_GENDER_MALE;
         level = 5;
     }
@@ -70,7 +74,11 @@ void LONG_CALL _CreateTradeMon(struct PartyPokemon *mon, struct NPCTrade *trade_
         nature = gf_rand() % 3;
         if (nature == 0) {
             trade_dat->give_species = SPECIES_ELECTRODE;
-            trade_dat->heldItem = ITEM_YACHE_BERRY;
+            trade_dat->heldItem = ITEM_KEBIA_BERRY;
+            ability = ABILITY_STATIC;
+            if (gf_rand() % 2 == 0) {
+                ability = ABILITY_AFTERMATH;
+            }
         } else if (nature == 1) {
             trade_dat->give_species = SPECIES_PAWMOT;
             trade_dat->heldItem = ITEM_SHUCA_BERRY;
