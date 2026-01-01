dac5e00b3a4273121a1f99024223c88545d8ea1a
kumakuma1 <kuehnmaxi@gmail.com>
2026-01-01 11:21:47 +0100

    -save guard the whitney fight by enabling the radio quiz after schorsch - remove weekday siblings - level cap/gym healing at pryce -disable mom saving money via phone

--- src/party_menu.c
+++ src/party_menu.c
@@ -242,3 +242,8 @@ BOOL LONG_CALL ov101_021EA804(void *mapApp UNUSED, u16 mapID UNUSED, u16 x UNUSE
 {
     return TRUE;
 }
+
+BOOL LONG_CALL PhoneCallPersistentState_MomGiftQueue_IsFull(void *callPersistentState UNUSED)
+{
+    return TRUE;
+}
