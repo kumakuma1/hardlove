cb3998485f5935c5077960921991a6b8c7b6765f
kumakuma1 <kuehnmaxi@gmail.com>
2026-07-22 07:33:43 +0200

    not breakable

--- src/individual/MoveHitDefenderAbilityCheck.c
+++ src/individual/MoveHitDefenderAbilityCheck.c
@@ -395,8 +395,8 @@ BOOL MoveHitDefenderAbilityCheckInternal(struct BattleSystem *bw, struct BattleS
             seq_no[0] = SUB_SEQ_COTTON_DOWN;
             ret = TRUE;
         }
-    } else if (MoldBreakerAbilityCheck(sp, sp->attack_client, sp->defence_client, ABILITY_PERISH_BODY)) {
-        //defender does need to be alive
+    } else if (GetBattlerAbility(sp, sp->defence_client) == ABILITY_PERISH_BODY) { // not breakable
+        //defender does not need to be alive
         if ((sp->oneSelfFlag[sp->defence_client].physical_damage || sp->oneSelfFlag[sp->defence_client].special_damage)
             && (IsContactBeingMade(GetBattlerAbility(sp, sp->attack_client), HeldItemHoldEffectGet(sp, sp->attack_client), HeldItemHoldEffectGet(sp, sp->defence_client), sp->current_move_index, sp->moveTbl[sp->current_move_index].flag))){
             sp->addeffect_type = ADD_EFFECT_PRINT_WORK_ABILITY;
